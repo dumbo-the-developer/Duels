7f33a7345e9404ad4de3fbd2e29da102d0444847
Sneyzi <30902875+Sneyzi@users.noreply.github.com>
2025-08-27 19:56:51 +0300

    fix: check only target player in TargetCheckMatchValidator

--- duels-plugin/src/main/java/com/meteordevelopments/duels/validator/validators/request/target/TargetCheckMatchValidator.java
+++ duels-plugin/src/main/java/com/meteordevelopments/duels/validator/validators/request/target/TargetCheckMatchValidator.java
@@ -1,15 +1,16 @@
 package com.meteordevelopments.duels.validator.validators.request.target;
 
-import java.util.Collection;
 import com.meteordevelopments.duels.DuelsPlugin;
 import com.meteordevelopments.duels.party.Party;
 import com.meteordevelopments.duels.util.function.Pair;
 import com.meteordevelopments.duels.validator.BaseTriValidator;
 import org.bukkit.entity.Player;
 
+import java.util.Collection;
+
 
 public class TargetCheckMatchValidator extends BaseTriValidator<Pair<Player, Player>, Party, Collection<Player>> {
-    
+
     private static final String MESSAGE_KEY = "ERROR.duel.already-in-match.target";
     private static final String PARTY_MESSAGE_KEY = "ERROR.party-duel.already-in-match.target";
 
@@ -19,11 +20,11 @@ public class TargetCheckMatchValidator extends BaseTriValidator<Pair<Player, Pla
 
     @Override
     public boolean validate(final Pair<Player, Player> pair, final Party party, final Collection<Player> players) {
-        if (players.stream().anyMatch(player -> arenaManager.isInMatch(player))) {
+        if (arenaManager.isInMatch(pair.getKey())) {
             lang.sendMessage(pair.getKey(), party != null ? PARTY_MESSAGE_KEY : MESSAGE_KEY, "name", pair.getValue().getName());
             return false;
         }
-        
+
         return true;
     }
 }
